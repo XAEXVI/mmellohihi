4054a84e90023dd128557c1213939848aa1f80c0
Caio Reinelt <cgxae16@gmail.com>
2023-06-28 00:31:30 -0300

    first

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.contrib import admin
+from django.urls import path, include  # add this
+
+urlpatterns = [
+    path('admin/', admin.site.urls),          # Django admin route
+
+    # ADD NEW Routes HERE
+
+    # Leave `Home.Urls` as last the last line
+    path("", include("apps.home.urls"))
+]
