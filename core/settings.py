d95b97a6a2238fedb00c972d35ea7dfc7f3793bd
Caio Reinelt <cgxae16@gmail.com>
2023-07-10 06:39:18 -0300

    dasdas

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
-STATIC_URL = 'https://mmellospace.sfo3.digitaloceanspaces.com/mmellostaticfiles/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
